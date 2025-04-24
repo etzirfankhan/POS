5ccb5d8e8d765d32f99a6d7e02505489fb250193
Irfan Khan <etzirfankhan@gmail.com>
2025-04-23 20:25:29 -0700

    Accessed User input inside server file
    todo#2
    creating users by adding create new account form and saving data to users file as accessed from user data submission.

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask,render_template,request
+import requests
 
+
+
+
+# Initialize the Flask application
 app = Flask(__name__)
 
+@app.route("/login",methods=["POST"])
+def receive_data():
+    name = request.form.get('username')
+    password = request.form.get('password')
+    return f"Name: {name}, Password: {password}"
+    
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
